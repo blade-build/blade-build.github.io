9724c840256bad022f44585db38eb507a73452e2
chen3feng <chen3feng@gmail.com>
2026-01-18 10:58:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo install.sh
+set -e
+
+mkdir -p ~/.cache
+
+if [[ -d ~/.cache/blade-build ]]; then
+    git -C ~/.cache/blade-build pull
+else
+    git clone https://github.com/blade-build/blade-build ~/.cache/blade-build
+fi
+
+cd ~/.cache/blade-build
+./install
+
